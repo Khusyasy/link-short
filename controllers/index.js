c5642afd0d87c87707333747938d86df8bf86440
Luthfi Khusyasy <Khusyasy@users.noreply.github.com>
2021-02-04 16:18:22 +0700

    change all query to be promise based

--- controllers/index.js
+++ controllers/index.js
@@ -6,62 +6,67 @@ exports.index = function (req, res, next) {
     res.render('index', { title: 'Link Shortener', msg: req.session.msg });
 }
 
-exports.create_link = function (req, res, next) {
+exports.create_link = async function (req, res, next) {
     var url = req.body.link;
     if (URLisValid(url)) {
         var hash = createShortID(url);
-        connection.query(`INSERT INTO links (ID, link_long, link_short, created, click) VALUES (null, '${escape(url)}', '${hash}', NOW(), 0)`, function (err, results) {
-            if (err) {
-                req.session.msg = { status: "error", text: "Connection Error" };
-                res.redirect("/");
-            } else {
-                res.redirect("/created/" + hash);
-            }
-        });
+        try {
+            var query_res = await connection.query(`INSERT INTO links (ID, link_long, link_short, created, click) VALUES (null, '${escape(url)}', '${hash}', NOW(), 0)`);
+            res.redirect("/created/" + hash);
+        } catch (err) {
+            req.session.msg = { status: "error", text: "Connection Error" };
+            res.redirect("/");
+        }
     } else {
         req.session.msg = { status: "error", text: "URL is invalid" };
         res.redirect("/");
     }
 }
 
-exports.get_link = function (req, res, next) {
-    connection.query(`SELECT * FROM links WHERE link_short = '${escape(req.params.hash)}'`, function (err, results, fields) {
-        if (err) {
-            req.session.msg = { status: "error", text: "Connection Error" };
-            res.redirect("/");
-        } else if (results[0]) {
-            res.render('link', { title: "Redirecting", link: unescape(results[0].link_long) });
-            connection.query(`UPDATE links SET click = ${results[0].click + 1} WHERE ID = ${results[0].ID}`, function (e, r) { if (e) console.log(e); });
+exports.get_link = async function (req, res, next) {
+    try {
+        var query_res = await connection.query(`SELECT * FROM links WHERE link_short = '${escape(req.params.hash)}'`);
+        link = query_res[0][0];
+        if (link) {
+            connection.query(`UPDATE links SET click = ${link.click + 1} WHERE ID = ${link.ID}`);
+            res.render('link', { title: "Redirecting", link: unescape(link.link_long) });
         } else {
             res.sendStatus(404);
         }
-    });
+    } catch (err) {
+        req.session.msg = { status: "error", text: "Connection Error" };
+        res.redirect("/");
+    }
 }
 
-exports.created = function (req, res, next) {
+exports.created = async function (req, res, next) {
     var hash = req.params.hash;
-    connection.query(`SELECT * FROM links WHERE link_short = '${escape(hash)}'`, function (err, results, fields) {
-        if (err) {
-            req.session.msg = { status: "error", text: "Connection Error" };
-            res.redirect("/");
-        } else if (results[0]) {
-            res.render('created', { title: "Created - " + unescape(results[0].link_long), long: unescape(results[0].link_long), short: process.env.BASE_URL + hash, stats: process.env.BASE_URL + "stats/" + hash });
+    try {
+        var query_res = await connection.query(`SELECT * FROM links WHERE link_short = '${escape(hash)}'`);
+        var link = query_res[0][0];
+        if (link) {
+            res.render('created', { title: "Created - " + unescape(link.link_long), long: unescape(link.link_long), short: process.env.BASE_URL + hash, stats: process.env.BASE_URL + "stats/" + hash });
         } else {
             res.sendStatus(404);
         }
-    });
+    } catch (err) {
+        req.session.msg = { status: "error", text: "Connection Error" };
+        res.redirect("/");
+    }
 }
 
-exports.view_link = function (req, res, next) {
+exports.view_link = async function (req, res, next) {
     var hash = req.params.hash;
-    connection.query(`SELECT * FROM links WHERE link_short = '${escape(hash)}'`, function (err, results, fields) {
-        if (err) {
-            req.session.msg = { status: "error", text: "Connection Error" };
-            res.redirect("/");
-        } else if (results[0]) {
-            res.render('stats', { title: "Created - " + unescape(results[0].link_long), long: unescape(results[0].link_long), short: process.env.BASE_URL + hash, click: results[0].click });
+    try {
+        var query_res = await connection.query(`SELECT * FROM links WHERE link_short = '${escape(hash)}'`);
+        var link = query_res[0][0];
+        if (link) {
+            res.render('stats', { title: "Created - " + unescape(link.link_long), long: unescape(link.link_long), short: process.env.BASE_URL + hash, click: link.click });
         } else {
             res.sendStatus(404);
         }
-    });
+    } catch (err) {
+        req.session.msg = { status: "error", text: "Connection Error" };
+        res.redirect("/");
+    }
 }
